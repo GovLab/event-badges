9e2a833de292c0f9c3d58acfe67f75f40f6972f3
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2017-05-04 15:49:57 -0400

    formatting tweaks

--- build.py
+++ build.py
@@ -15,10 +15,10 @@ outputpath  = os.path.join(cwd, "site")
 # We load the data we want to use in the templates.
 #BADGES    = yaml.load(open('data/badges.yaml'))
 
-SECTOR_MAPPING = { 'Government': 'gov',
-'Business': 'com',
-'Nonprofit': 'org',
-'Academia': 'edu' }
+SECTOR_MAPPING = { 'Industry Professional': 'com',
+	'Graduate Student': 'grad',
+	'High School Student': 'highschool',
+	'Undergraduate Student': 'undergrad',}
 
 def getSector(str):
 	if str in SECTOR_MAPPING:
@@ -28,36 +28,36 @@ def getSector(str):
 
 import csv
 
+
 BADGES = []
-with open('data/__badges2.csv', 'rb') as csvfile:
+with open('/Users/sahuguet/Downloads/WiTNY Build-a-Thon Participants.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
-		(fname,lname,org,title,workshop,subsector,email,tbd,speaker) = row
+		print(len(row))
+		(timestamp, fname,	lname,	category, email, grade_level,
+		xp_python, xp_APIs, xp_JavaScript, xp_nodejs, xp_HTML, xp_CPP, xp_pitching, xp_project_m, xp_engineering,
+		extra_info, skip, focus, employer, laptop,
+		ugrad_vs_grad, which_cuny, major, ct_program, tshirt_size, dietary_restrictions, school, cs) = row
+		#(ts, fname, lname, category,
+		#email, school, grade_level, cs_class, undergrad_vs_grad,
+		#which_cuny, cuny_major, ct_programm, professional_focus, company,
+		#python_expertise, javascript_expertise, html_expertise, cpp_expertise, nodejs_expertise, api,
+		#ability_pitch, ability_project_management, ability_eng,
+		#will_bring_laptop, diet, t_shirt, extra_experience, one_more) = row
 		BADGES.append( { 'fname': fname,
 			'lname': lname,
-			'org': org,
-			'title': title,
-			'sector': getSector(subsector),
-			'job': title,
-			'field': workshop,
-			'speaker': speaker })
+			'org': employer,
+			'cuny': which_cuny,
+			'employer': employer + school + which_cuny,
+			'job': '',
+			'title': 'no title',
+			'sector': getSector(category),})
 
 BADGES = sorted(BADGES, key=lambda x:x['lname'])
 
-for sector in ['gov', 'edu', 'com', 'org']:
-	for i in range(1,6):
-		BADGES.append( { 'fname': '',
-			'lname': '',
-			'org': '',
-			'title': '',
-			'sector': sector,
-			'job': '',
-			'field': 'TBD',
-			'speaker': '' })
-
 def loadAcademyData():
 	return { 'badges': BADGES,
-					 'resources': None }
+			 'resources': None }
 
 site = staticjinja.make_site(
 	searchpath=searchpath,
